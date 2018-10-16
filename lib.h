5905567598649a8d1e6d3b2b67294a677ec9860f
Mandy <mmai1@stuy.edu>
2018-10-16 15:30:26 -0400

    update

--- lib.h
+++ lib.h
@@ -14,11 +14,18 @@ struct song_node * lib[27];
 
 struct song_node * add(song_node);
 
-search_song();
-search_artist();
-print_letter();
-print_artist();
-print_lib();
-shuffle();
-delete();
-clear();
+struct song_node * search_song();
+
+struct song_node * search_artist();
+
+void print_letter();
+
+void print_artist();
+
+void print_lib();
+
+void shuffle();
+
+struct song_node * delete_song();
+
+struct song_node * clear();
